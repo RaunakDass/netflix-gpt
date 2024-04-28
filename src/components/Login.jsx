8ed54d2f40f6ab2d3965bcaf76af5814b83e9a80
Raunak Das <dasraunak5@gmail.com>
2024-04-28 14:55:41 +0530

    Changes to Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Login = () => {
+    const [isSignInForm, setIsSignInForm] = useState(true);
+    const toggleSignInForm = () => {
+        setIsSignInForm(!isSignInForm);
+    };
+    
   return (
     <div>Login</div>
   )
